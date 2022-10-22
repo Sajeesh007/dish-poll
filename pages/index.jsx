2418dc2952bfdf3806d767e118b57d0e8fdb7ece
Sajeesh007 <sajeesh8112001@gmail.com>
2022-10-22 11:34:10 +0530

    added media queries

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,12 +20,12 @@ export default function Home() {
 
   return (
     <div className='flex flex-col pt-4  items-center min-h-screen bg-indigo-200'>
-      <h2 className="text-5xl pb-4">Dishes</h2>
+      <h2 className="text-5xl pb-4 font-bold">Dishes</h2>
       <Tab />
-      <div className="grid grid-cols-4 gap-10 py-6">
+      <div className="grid  grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 py-6">
         {dish?.map((item) => 
           <DishCard key={item.id} id={item.id} name={item.dishName} desc={item.description} image={item.image} rank={item.rank} dishDispatch={dishDispatch}/>
-          )}
+        )}
       </div>
     </div>
   )
